f2dc5d8192060145727438ff55695968a30c7886
goraedoll <goraedoll21@gmail.com>
2025-09-16 15:33:07 +0900

    init

--- RunMung/RunMungApp.swift
+++ RunMung/RunMungApp.swift
@@ -2,31 +2,16 @@
 //  RunMungApp.swift
 //  RunMung
 //
-//  Created by 고래돌 on 9/10/25.
+//  Created by 고래돌 on 9/16/25.
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct RunMungApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView()   // 앱 실행 시 가장 먼저 띄우는 화면
         }
-        .modelContainer(sharedModelContainer)
     }
 }
